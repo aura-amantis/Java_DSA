a8ca1d2b6866cea331e59f52f271043d6163b346
Tisha Jain <103413370+aura-amantis@users.noreply.github.com>
2023-01-22 19:25:19 +0530

    Inverted Half Pyramid

---
+++ Patterns/InvHalfPyr.java
@@ -0,0 +1,24 @@
+//code to form an inverted half pyramid
+
+import java.util.*;
+
+public class InvHalfPyr {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter number of rows...");
+       int n = sc.nextInt();
+
+       //row no. = total columns
+      
+       //outer loop
+       for(int i = n ; i>=1; i--){
+           //inner loop
+           for(int j = 1; j<=i; j++){
+              System.out.print("*");
+           }
+           System.out.println();
+       }
+   }
+}
+
+
