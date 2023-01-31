7c9d0fd39ba094d92f3bda816477f1e3f26c04d3
Tisha Jain <103413370+aura-amantis@users.noreply.github.com>
2023-01-31 09:33:49 +0530

    Hollow Rhombus

---
+++ Patterns/HollowRhombus.java
@@ -0,0 +1,33 @@
+//code to form a Hollow Rhombus
+
+import java.util.*;
+
+public class HollowRhombus {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows...");
+        int n = sc.nextInt();
+    
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n-i; j++) {
+                System.out.print(" ");
+            }
+
+            if(i==1 || i==n){
+                for(int j=1; j<=n; j++) {
+                    System.out.print("*");
+                }
+            }else{
+                for(int j=1; j<=n; j++) {
+                    if(j==1 || j==n){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
